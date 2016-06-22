0e0556cf79e1b1d0c0c385e20f41c1fcece80ddd
Jeff Dickey <dickeyxxx@gmail.com>
2016-06-22 10:32:20 -0700

    [WIP] get postgreses via attachment

--- commands/info.js
+++ commands/info.js
@@ -3,6 +3,16 @@
 const co = require('co')
 const cli = require('heroku-cli-util')
 
+function configVarNamesFromValue (config, value) {
+  const sortBy = require('lodash.sortby')
+
+  let keys = []
+  for (let key of Object.keys(config)) {
+    if (config[key] === value) keys.push(key)
+  }
+  return sortBy(keys, k => k !== 'DATABASE_URL', 'name')
+}
+
 function databaseNameFromUrl (uri, config) {
   const invert = require('lodash.invert')
   const url = require('url')
@@ -15,55 +25,59 @@ function databaseNameFromUrl (uri, config) {
 }
 
 function displayDB (db) {
-  cli.styledHeader(cli.color.attachment(db.addon.name))
-  let info = db.info.info.reduce((info, i) => {
+  cli.styledHeader(db.configVars.map(c => cli.color.configVar(c)).join(', '))
+  db.db.info.push({name: 'Add-on', values: [db.addon.name]})
+  let info = db.db.info.reduce((info, i) => {
     if (i.values.length > 0) {
       info[i.name] = i.resolve_db_name ? databaseNameFromUrl(i.values[0], db.config) : i.values.join(', ')
     }
     return info
   }, {})
-  info['Config Vars'] = db.addon.config_vars.map((c) => cli.color.configVar(c)).join(', ')
-  let keys = ['Config Vars'].concat(db.info.info.map((i) => i.name))
+  let keys = db.db.info.map(i => i.name)
   cli.styledObject(info, keys)
   cli.log()
 }
 
-function * run (context, heroku) {
+function * run (context) {
+  const sortBy = require('lodash.sortby')
   const host = require('../lib/host')
   const fetcher = require('../lib/fetcher')
   const app = context.app
   const db = context.args.database
 
   let addons = []
-  let config = heroku.get(`/apps/${app}/config-vars`)
+  let config = cli.heroku.get(`/apps/${app}/config-vars`)
 
   if (db) {
-    addons = yield [fetcher.addon(heroku, app, db)]
+    addons = yield [fetcher.addon(app, db)]
   } else {
-    addons = yield fetcher.all(heroku, app)
+    addons = yield fetcher.all(app)
     if (addons.length === 0) {
       cli.log(`${cli.color.app(app)} has no heroku-postgresql databases.`)
       return
     }
   }
 
-  let dbs = yield addons.map((a) => {
+  let dbs = yield addons.map(addon => {
     return {
-      addon: a,
+      addon,
       config,
-      info: heroku.request({
-        host: host(a),
+      db: cli.heroku.request({
+        host: host(addon),
         method: 'get',
-        path: `/client/v11/databases/${a.name}`
+        path: `/client/v11/databases/${addon.name}`
       })
     }
   })
+
+  dbs.forEach(db => { db.configVars = configVarNamesFromValue(db.config, db.db.resource_url) })
+  dbs = sortBy(dbs, db => db.configVars[0] !== 'DATABASE_URL', 'configVars[0]')
 
   dbs.forEach(displayDB)
 }
 
 let cmd = {
-  topic: 'pg',
+  topic: '_pg',
   needsApp: true,
   needsAuth: true,
   args: [{name: 'database', optional: true}],
