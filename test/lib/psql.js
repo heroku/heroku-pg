a2b3b76125fe20c75a6aac58e754e64bf099f465
Camille Baldock <camille@heroku.com>
2016-11-22 14:32:10 +0000

    Test bastion path spawns too

--- test/lib/psql.js
+++ test/lib/psql.js
@@ -10,7 +10,19 @@ const db = {
   password: 'pass',
   database: 'mydb',
   port: 5432,
-  host: 'localhost'
+  host: 'localhost',
+  hostname: 'localhost'
+}
+
+const bastionDb = {
+  user: 'jeff',
+  password: 'pass',
+  database: 'mydb',
+  port: 5432,
+  bastionHost: 'bastion-host',
+  bastionKey: 'super-private-key',
+  host: 'localhost',
+  hostname: 'localhost'
 }
 
 describe('psql', () => {
@@ -42,6 +54,36 @@ describe('psql', () => {
         }
       )
       return psql.exec(db, 'SELECT NOW();', 1000)
+      .then(() => cp.verify())
+      .then(() => cp.restore())
+    }))
+    it('runs psql for bastion databases', sinon.test(() => {
+      let cp = sinon.mock(require('child_process'))
+      let env = Object.assign({}, process.env, {
+        PGAPPNAME: 'psql non-interactive',
+        PGSSLMODE: 'prefer',
+        PGUSER: 'jeff',
+        PGPASSWORD: 'pass',
+        PGDATABASE: 'mydb',
+        PGPORT: 5432,
+        PGHOST: 'localhost'
+      })
+      let opts = {env: env, encoding: 'utf8'}
+      let onHandler = function (key, data) {
+        return Promise.resolve('result')
+      }
+      cp.expects('spawn').withArgs('psql', ['-c', 'SELECT NOW();']).once().returns(
+        {
+          stdout: {
+            on: onHandler
+          },
+          stderr: {
+            on: onHandler
+          },
+          on: onHandler
+        }
+      )
+      return psql.exec(bastionDb, 'SELECT NOW();', 1000)
       .then(() => cp.verify())
       .then(() => cp.restore())
     }))
