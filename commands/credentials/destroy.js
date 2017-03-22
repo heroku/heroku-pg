cc256c1fcedcbedf1b92643fce4a951525dd73a6
Maciek Sakrejda <maciek@heroku.com>
2017-03-22 10:45:54 -0700

    Add credentials:destroy

---
+++ commands/credentials/destroy.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const co = require('co')
+const cli = require('heroku-cli-util')
+
+function * run (context, heroku) {
+  const fetcher = require('../../lib/fetcher')(heroku)
+  const host = require('../../lib/host')
+
+  const {app, args, flags} = context
+  let cred = flags.name
+
+  let db = yield fetcher.addon(app, args.database)
+
+  yield cli.confirmApp(app, flags.confirm, `WARNING: Destructive action`)
+
+  yield cli.action(`Destroying credential ${cli.color.cmd(cred)}`, co(function * () {
+    yield heroku.delete(`/postgres/v0/databases/${db.name}/credentials/${cred}`, {host: host(db)})
+  }))
+
+  cli.log(`The credential has been destroyed within ${db.name} and detached from all apps.`)
+  cli.log(`Database objects owned by ${cred} will be assigned to the default credential`)
+}
+
+module.exports = {
+  topic: 'pg',
+  command: 'credentials:destroy',
+  description: 'destroy role within database',
+  needsApp: true,
+  needsAuth: true,
+  help: `
+Example Usage:
+  heroku pg:credentials:destroy postgresql-something-12345 --name role_to_destroy
+`,
+  args: [{name: 'database', optional: true}],
+  flags: [
+    {name: 'name', hasValue: true, required: true, description: "name of credential to destroy"},
+    {name: 'confirm', char: 'c', hasValue: false}
+  ],
+  run: cli.command({preauth: true}, co.wrap(run))
+}
---
+++ test/commands/credentials/destroy.js
@@ -0,0 +1,55 @@
+'use strict'
+/* global describe it beforeEach afterEach */
+
+const cli = require('heroku-cli-util')
+const expect = require('unexpected')
+const nock = require('nock')
+const proxyquire = require('proxyquire')
+
+const db = {
+  database: 'mydb',
+  host: 'foo.com',
+  user: 'jeff',
+  password: 'pass',
+  url: {href: 'postgres://jeff:pass@foo.com/mydb'}
+}
+
+const addon = {
+  name: 'postgres-1',
+  plan: {name: 'heroku-postgresql:standard-0'}
+}
+
+const fetcher = () => {
+  return {
+    database: () => db,
+    addon: () => addon
+  }
+}
+
+const cmd = proxyquire('../../../commands/credentials/destroy', {
+  '../../lib/fetcher': fetcher
+})
+
+describe('pg:credentials:destroy', () => {
+  let api, pg
+
+  beforeEach(() => {
+    api = nock('https://api.heroku.com')
+    pg = nock('https://postgres-api.heroku.com')
+    cli.mockConsole()
+  })
+
+  afterEach(() => {
+    nock.cleanAll()
+    api.done()
+  })
+
+  it('destroys the credential', () => {
+    pg.delete('/postgres/v0/databases/postgres-1/credentials/credname').reply(200)
+    return cmd.run({app: 'myapp', args: {}, flags: {name: "credname", confirm: 'myapp'}})
+    .then(() => expect(cli.stderr, 'to equal', 'Destroying credential credname... done\n'))
+    .then(() => expect(cli.stdout, 'to equal', `The credential has been destroyed within postgres-1 and detached from all apps.
+Database objects owned by credname will be assigned to the default credential
+`))
+  })
+})
