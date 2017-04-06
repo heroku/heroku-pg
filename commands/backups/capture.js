f055ffd87700240b50ba65e3838aa2de290f9a73
Keiko Oda <keiko713@gmail.com>
2017-04-06 16:10:16 -0700

    Add/fix tests

--- commands/backups/capture.js
+++ commands/backups/capture.js
@@ -21,7 +21,7 @@ Use Ctrl-C at any time to stop monitoring progress; the backup will continue run
 Use ${cli.color.cmd('heroku pg:backups:info')} to check progress.
 Stop a running backup with ${cli.color.cmd('heroku pg:backups:cancel')}.
 `)
-  if (context.app != db.app.name) {
+  if (app !== db.app.name) {
     cli.log(`HINT: You are running this command with a non-billing application.
 Use ${cli.color.cmd('heroku pg:backups -a ' + db.app.name)} to check the list of backups.
 `)
