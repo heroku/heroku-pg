8a62916f8bb2d0c26cd4fd80eee1459b0032f1f0
Gabriel Enslein <genslein@genslein-ltm.internal.salesforce.com>
2017-08-08 18:36:38 -0400

    correct code styling on new warning test

--- test/commands/credentials/rotate.js
+++ test/commands/credentials/rotate.js
@@ -196,11 +196,11 @@ This command will affect the apps appname_1, appname_2, appname_3.`
       args: {},
       flags: { all: true, force: true, confirm: 'myapp' }})
       .then(() => {
-      expect(lastApp, 'to equal', 'myapp')
-      expect(lastConfirm, 'to equal', 'myapp')
-      expect(lastMsg, 'to equal', message)
+        expect(lastApp, 'to equal', 'myapp')
+        expect(lastConfirm, 'to equal', 'myapp')
+        expect(lastMsg, 'to equal', message)
+      })
     })
-  })
 
   it('requires app confirmation for rotating a specific role with --name', () => {
     pg.post('/postgres/v0/databases/postgres-1/credentials/my_role/credentials_rotation').reply(200)
