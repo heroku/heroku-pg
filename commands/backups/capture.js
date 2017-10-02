59fe7eb9e7a9c52a0e3731148ac5e68f4dc084b0
Corey Purcell <me@coreypurcell.com>
2017-10-02 16:28:52 -0400

    exit if we can't find the db

--- commands/backups/capture.js
+++ commands/backups/capture.js
@@ -18,13 +18,13 @@ function * run (context, heroku) {
     path: `/client/v11/databases/${db.id}`
   }).catch(err => {
     if (err.statusCode !== 404) throw err
-    cli.warn(`${cli.color.addon(db.name)} is not yet provisioned.\nRun ${cli.color.cmd('heroku addons:wait')} to wait until the db is provisioned.`)
+    cli.exit(1, `${cli.color.addon(db.name)} is not yet provisioned.\nRun ${cli.color.cmd('heroku addons:wait')} to wait until the db is provisioned.`)
   })
 
   if (dbInfo) {
     let dbProtected = /On/.test(dbInfo.info.find(attribute => attribute.name === 'Continuous Protection').values[0])
     if (dbProtected) {
-      yield cli.confirmApp(app, flags.confirm, 'Continuous protection is already enabled for this database. Logical backups of large databases are likely to timeout. Are you sure?')
+      yield cli.confirmApp(app, flags.confirm, 'Continuous protection is already enabled for this database. Logical backups of large databases are likely to timeout. See https://devcenter.heroku.com/articles/heroku-postgres-data-safety-and-continuous-protection#physical-backups-on-heroku-postgres.  Are you sure?')
     }
   }
 
