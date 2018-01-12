461a703ef3dac0e17788f71e58d68fc5d0b1a9f1
Jon Daniel <binarycleric@gmail.com>
2018-01-12 16:27:23 -0500

    Refactoring command to hit shogun for connection pooling checks
    It is better to hit shogun to make sure everyone in pgbouncer land is good
    before trying to attach the addon. More server-side logic and less worrying
    about CLI updates.

--- commands/connection_pooling.js
+++ commands/connection_pooling.js
@@ -9,48 +9,39 @@ function * run (context, heroku) {
   const fetcher = require('../lib/fetcher')(heroku)
   const {app, args, flags} = context
 
-  let addon = yield heroku.get(`/addons/${encodeURIComponent(args.database)}`)
-
   let db = yield fetcher.addon(app, args.database)
+  let addon = yield heroku.get(`/addons/${encodeURIComponent(db.name)}`)
+  let credential = `${flags.credential || 'default'}`
+
   if (util.starterPlan(db)) throw new Error('This operation is not supported by Hobby tier databases.')
 
-  function createAttachment (app, as, credential) {
-    let body = {
-      name: as,
-      app: {name: app},
-      addon: {name: addon.name},
+  function activatePoolCredential(db, credential) {
+    return heroku.request({
+      host: host(db),
+      method: 'POST',
+      path: `/client/v11/databases/${db.name}/connection-pooling/credentials/${credential}`
+    })
+  }
+
+  function createAttachment (addon, app, db, credential) {
+    let attachmentParams = {
+      app: { name: app },
+      addon: { name: addon.name },
       namespace: `connection-pooling:${credential}`
     }
+
     return cli.action(
-      `Attaching ${credential ? cli.color.addon(credential) + ' of ' : ''}${cli.color.addon(addon.name)}${as ? ' as ' + cli.color.attachment(as) : ''} to ${cli.color.app(app)}`,
+      `Enabling Connection Pooling for credential ${cli.color.addon(credential)} on ${cli.color.addon(addon.name)} to ${cli.color.app(app)}`,
       heroku.request({
         path: '/addon-attachments',
         method: 'POST',
-        body: body
+        body: attachmentParams
       })
     )
   }
 
-  let pgbouncerConfig = yield heroku.request({host: host(addon), method: 'get', path: `/client/v11/databases/${addon.id}/pgbouncer_status`})
-  if (pgbouncerConfig.status === 'disabled') {
-    throw new Error(`The database ${addon.name} does not have connection pooling enabled`)
-  }
-
-  let credential = `${flags.credential || 'default'}`
-
-  if (credential !== 'default') {
-    let credentialConfig = yield heroku.get(`/addons/${addon.name}/config/credential:${encodeURIComponent(credential)}`)
-    if (credentialConfig.length === 0) {
-      throw new Error(`Could not find credential ${credential} for database ${addon.name}`)
-    }
-  }
-
-  let poolingConfig = yield heroku.get(`/addons/${addon.name}/config/connection-pooling:${encodeURIComponent(credential)}`)
-  if (poolingConfig.length === 0) {
-    throw new Error(`Could not find credential ${credential} with connection pooling for database ${addon.name}`)
-  }
-
-  let attachment = yield createAttachment(app, flags.as, credential)
+  let pool = yield activatePoolCredential(db, credential)
+  let attachment = yield createAttachment(addon, app, db, credential)
 
   yield cli.action(
     `Setting ${cli.color.attachment(attachment.name)} config vars and restarting ${cli.color.app(app)}`,
